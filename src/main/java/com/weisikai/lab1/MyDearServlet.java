887dddcdd36e63f0af73cf707d54f139ea480a04
2020211001000628WeiSikai <1745081605@qq.com>
2022-05-16 22:03:20 +0800

    lab1 code

---
+++ src/main/java/com/weisikai/lab1/MyDearServlet.java
@@ -0,0 +1,39 @@
+package com.weisikai.lab1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/MyDearServletURL")
+public class MyDearServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        String name = req.getParameter("name").trim();
+        System.out.printf(name);
+        String _class = req.getParameter("class").trim();
+        String ID = req.getParameter("ID").trim();
+        String submit = req.getParameter("submit").trim();
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+
+        out.print("<html>\n" +
+                "<head>\n" +
+                "<meta charset=\"UTF-8\">"+
+                "<title>MyDearServlet</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "    name:"+name+"<br/>\n" +
+                "    submit:"+submit+"<br/>\n"+
+                "    class:"+_class+"<br/>\n" +
+                "    ID:"+ID+"<br/>\n" +
+                "</body>\n" +
+                "</html>");
+        out.close();
+    }
+
+}
---
+++ src/main/webapp/lab1/MyDearJsp.jsp
@@ -0,0 +1,29 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: KAI
+  Date: 2022/5/16
+  Time: 20:20
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>MyDearJsp.jsp</title>
+</head>
+<body>
+<%
+    request.setCharacterEncoding("UTF-8");
+%>
+<h2>The get result by java code</h2>
+name:<%=request.getParameter("name")%><br/>
+submit:<%=request.getParameter("submit")%><br/>
+class:<%=request.getParameter("_class")%><br/>
+ID:<%=request.getParameter("ID")%><br/>
+<hr/>
+<h2>The get result by EL</h2>
+name:${param.name}<br/>
+submit:${param.submit}<br/>
+class:${param._class}<br/>
+ID:${param.ID}<br/>
+</body>
+</html>
